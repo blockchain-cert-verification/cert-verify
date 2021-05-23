98299139749a7de0520c793f055a1172b801963f
himanshi789 <himanshi.july@gmail.com>
2021-05-23 13:19:01 +0530

    form updation

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
+
 const Wrapper = styled.div`
   margin-top: 1em;
   margin-left: 6em;
@@ -8,6 +9,25 @@ const Wrapper = styled.div`
   `;
 export const Home = (props) => (
   <Wrapper>
-    <h2>This is a paragraph and I am writing on the home page</h2>
+     
+    <div>
+       <h1>Block Verifier</h1>
+          <div class="panel__block"></div>
+            <p>
+              A Blockchain based Certificate verification with Distributed storage. <br/>
+              The certificates will be immutable, confidential and tamperproof.<br/>
+              <br/><br/>
+              By combining 
+              <br/><br/>
+              BLOCKCHAIN + IPFS + ENCRYPTION = BLOCK VERIFIER
+              <br/><br/>
+              Combining the multiple technologies and elimination ones draw back with other
+              <br/><br/>
+              BLOCKCHAIN - Tamperproof<br/><br/>
+              IPFS       - Distributed Storage<br/><br/>
+              ENCRYPTION - confidentiality<br/><br/>
+            </p>
+          
+    </div>   
   </Wrapper>
 )
---
+++ app/src/SubmitAnimation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import "../Styles/submitAnimation.css";
+
+function SubmitAnimation(props) {
+  const { currentState } = props;
+  return (
+    <div className="container">
+        
+      <button type="button" name="Submit" value="submit" className={`animatedButton ${currentState}`} />
+    </div>
+  );
+}
+
+SubmitAnimation.propTypes = {
+  currentState: PropTypes.string.isRequired
+};
+
+export default SubmitAnimation;
